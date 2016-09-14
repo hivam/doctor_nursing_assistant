56f45463f84b90b313fd9fc1b70aaa68a13dad6f
camilozuluaga <juancamilozuluagaserna@hotmail.com>
2016-09-13 21:08:32 -0500

    agregando cambios, pasando el paciente para que se pueda generar bien la atencion

--- models/doctor_attentions_co_inherit.py
+++ models/doctor_attentions_co_inherit.py
@@ -38,18 +38,61 @@ class doctor_co_attentios(osv.osv):
 
 
 
+	def write(self, cr, uid, ids, vals, context=None):
+		lista_diagnostico = []
+		id_paciente = None
+		nombre_diagnostico = ''
+		ejcutar_write = None
+
+		if 'remite_aux_enfermeria' in vals:
+			if vals['remite_aux_enfermeria']:
+		
+				if not 'patient_id' in vals:
+					id_paciente = context['default_patient_id']
+				else:
+					id_paciente = vals['patient_id']
+
+				fecha_nacimiento =  self.pool.get('doctor.patient').browse(cr, uid, id_paciente, context=context).birth_date
+				res={}
+				res['patient_id'] =  id_paciente
+				res['conducta_medico'] = vals['conduct']
+				res['age_attention'] = self.calcular_edad(fecha_nacimiento)
+				res['age_unit'] = self.calcular_age_unit(fecha_nacimiento)
+
+				ejcutar_write = super(doctor_co_attentios,self).write(cr, uid, ids, vals, context)
+
+				if 'diseases_ids' in vals:
+					for i in range(0,len(vals['diseases_ids']),1):
+						lista_diagnostico.append(vals['diseases_ids'][i][2]['diseases_id'])
+						
+					if len(lista_diagnostico) > 0:
+						for i in lista_diagnostico:
+
+							nombre_diagnostico += '\n' + self.pool.get('doctor.diseases').browse(cr, uid, i, context=context).name
+
+					res['diagnostico_medico'] = nombre_diagnostico
+					
+				res['origin'] = vals['number']
+				self.pool.get('doctor.nursing.assistan').create(cr, uid, res, context)
+			else:
+				return super(doctor_co_attentios,self).write(cr, uid, ids, vals, context)
+		
+		return ejcutar_create
+
+
 	def create(self, cr, uid, vals, context=None):
 		lista_diagnostico = []
 		id_paciente = None
 		nombre_diagnostico = ''
 		ejcutar_create = None
+		_logger.info(context)
 		if 'remite_aux_enfermeria' in vals:
 			if vals['remite_aux_enfermeria']:
 				#id_especialidad = self.pool.get('doctor.professional').browse(cr, uid, vals['aux_enfermeria_id'], context=context).speciality_id.id
-				if not 'patient_id' in vals:
+				if not 'patient_id' in context:
 					id_paciente = context['default_patient_id']
 				else:
-					id_paciente = vals['patient_id']
+					id_paciente = context['patient_id']
 
 				fecha_nacimiento =  self.pool.get('doctor.patient').browse(cr, uid, id_paciente, context=context).birth_date
 				res={}
